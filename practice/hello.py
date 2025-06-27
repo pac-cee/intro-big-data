3af86dc9486656615879800fd3d3b22947cf5743
pac-cee <pacitekno12@gmail.com>
2025-06-27 16:44:49 +0200

    learning python

---
+++ practice/hello.py
@@ -0,0 +1,6 @@
+def convert ():
+    cel= float(input("enter the temp in c:\n "))
+    feh=(cel*9/5)+32
+    print("the insterted temp in c =",cel)
+    print(" the convertes temp in f =",feh)
+convert ()
